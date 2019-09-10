1dd9e6e451789b8ffd5571ab95d40eb62f7b5b47
andrew15722 <andrew15722@yahoo.com>
2019-09-10 13:08:48 -0700

    Met MVP goals

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 // * Context Api
 import { ProductContext } from './contexts/ProductContext.js';
+import { CartContext } from './contexts/CartContext.js';
 
 function App() {
 	const [ products ] = useState(data);
@@ -20,14 +21,16 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
-			<div className="App">
-				<Navigation cart={cart} />
+			<CartContext.Provider value={{ cart, setCart }}>
+				<div className="App">
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route exact path="/" component={products} />
+					{/* Routes */}
+					<Route exact path="/" component={Products} />
 
-				<Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
-			</div>
+					<Route path="/cart" component={ShoppingCart} />
+				</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
